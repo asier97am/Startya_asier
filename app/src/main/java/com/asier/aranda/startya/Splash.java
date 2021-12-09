99383eaa3932feb7ccceae8a33f9c2a738ca8bd6
Asier Aranda <asier.aranda.malagon@gmail.com>
2021-12-09 09:17:24 +0100

    poniendo fondo logo y cambiar al main automatico

--- app/src/main/java/com/asier/aranda/startya/Splash.java
+++ app/src/main/java/com/asier/aranda/startya/Splash.java
@@ -23,7 +23,12 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        openApp(true);
 
+        //implementar animacion  ------------------LOGO PRINCIPAL------------------
+        ImageView mLogo = (ImageView) findViewById(R.id.logoprincipal);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        mLogo.startAnimation(myanim);//inicio Animacion
 
         ImageView mback =findViewById(R.id.fondoSplash);
         Glide.with(this)
@@ -31,5 +36,18 @@ public class Splash extends AppCompatActivity {
                 .placeholder(new ColorDrawable(this.getResources().getColor(R.color.naranjaExam)))
                 .centerCrop()
                 .into(mback);
+    }
+
+    private void openApp(boolean locationPermission) {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //aqui indicamos que vaya del Splash al login
+                Intent intent = new Intent(Splash.this, com.asier.aranda.startya.MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }, 3000);//tarda 3s en pasar al siguiente activity
     }
 }
